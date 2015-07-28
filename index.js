9e1b7e9eb6a8e7c0d2fef6ba4434af43f27f3203
Brijesh kothari <brijesh.kothari@novanet.net>
2015-07-28 17:35:26 +0530

    changes for output with dynamic values without default values

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-var regex = require('./regex_for_numeric_range.js'),
-	min = 1000,
-	max = 1009;
+var regex = require('./regex_for_numeric_range.js');
 
 exports.regexGen = function(min_, max_){
-	if(!min_ && !max_){
-		console.log("Min - Max : ("+min+"-"+max+") : " , regex.regex_for_range(1000, 1009));
-	}
 	return regex.regex_for_range(min_, max_);
 }
