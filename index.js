51d7f6e4adcd0e8f4192e17ad956294a96a79ef4
Brijesh kothari <brijesh.kothari@novanet.net>
2015-07-28 17:20:32 +0530

    output changes

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ var regex = require('./regex_for_numeric_range.js'),
 	min = 1000,
 	max = 1009;
 
-export.regexGen = function(min_, max_){
+exports.regexGen = function(min_, max_){
 	console.log("Min - Max : ("+min+"-"+max+") : " , regex.regex_for_range(1000, 1009));
 	return regex.regex_for_range(1000, 1009);
 }
+
+
+var regNum = require('regex-range'),
+	min = 1500,
+	max = 2500;
+
+console.log(regNum.regexGen(min, max));	
