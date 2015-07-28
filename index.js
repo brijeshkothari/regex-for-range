7da3af933a01975c4eb07cf583229cbadd0b61e2
Brijesh kothari <brijesh.kothari@novanet.net>
2015-07-28 15:43:38 +0530

    changes for output specific

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 var regex = require('./regex-for-range.js');
 
-exports.printMsg = function() {
-  console.log("This is a npm library for regex generator for numeric range values");
-}
-
 exports.regexGen = function(min, max){
 	return regex(min, max);
 }
